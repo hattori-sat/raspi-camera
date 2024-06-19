a2818d2740ccff4fa504fa9c1b253bffb211602e
hattori <hattori.startup@gmail.com>
2024-06-20 00:28:03 +0900

    change cpp file name

--- function_test/main.cpp
+++ function_test/main.cpp
@@ -1,23 +1,29 @@
 #include <libcamera/libcamera.h>
 #include <iostream>
+
 using namespace libcamera;
 
-int main(){
+int main() {
     CameraManager cm;
     cm.start();
-    if (cm.cameras().empty()){
+
+    if (cm.cameras().empty()) {
         std::cerr << "No cameras available" << std::endl;
         return -1;
     }
+
     std::shared_ptr<Camera> camera = cm.cameras()[0];
-    if (camera->acquire()){
+    if (camera->acquire()) {
         std::cerr << "Failed to acquire camera" << std::endl;
         return -1;
     }
+
     std::cout << "Camera acquired: " << camera->id() << std::endl;
 
-    // ここでカメラの設定やキャプチャ処理を追加します
+    // ここでカメラの設定やキャプチャ処理を追加します
+
     camera->release();
     cm.stop();
+
     return 0;
 }
